81f2895f2e0bd17c2dcc88da05ca5ca57303505e
Justin Borodinsky <justin.borodinsky@gmail.com>
2015-04-04 11:18:32 -0500

    formatting

--- src/dftthread.h
+++ src/dftthread.h
@@ -10,37 +10,38 @@
 namespace PCMDFT
 {
 
-    class PCMSettings;
+class PCMSettings;
 
-    class DFTThread : public QObject
-    {
-        Q_OBJECT
-    public:
-        DFTThread (std::shared_ptr<const PCMSettings> spSettings, QObject* parent = 0, std::size_t clPlatId = 0, std::size_t clDeviceId = 0);
-        ~DFTThread();
+class DFTThread : public QObject
+{
+    Q_OBJECT
+public:
+    DFTThread (std::shared_ptr<const PCMSettings> spSettings, QObject* parent = 0,
+                std::size_t clPlatId = 0, std::size_t clDeviceId = 0);
+    ~DFTThread();
 
-        static QStringList getPlatformList();
-        static QStringList getDeviceList (std::size_t platformId);
+    static QStringList getPlatformList();
+    static QStringList getDeviceList (std::size_t platformId);
 
-        void waitForThread();
+    void waitForThread();
 
-    public slots:
-        void slotQuit();
-        void slotTimeSeriesUpdate (QByteArray bytes);
+public slots:
+    void slotQuit();
+    void slotTimeSeriesUpdate (QByteArray bytes);
 
-    signals:
-        void sigFreqCompReady (QByteArray tsBytes, QByteArray fcBytes);
-        void sigError (QString value);
-        void sigDebug (QString value);
+signals:
+    void sigFreqCompReady (QByteArray tsBytes, QByteArray fcBytes);
+    void sigError (QString value);
+    void sigDebug (QString value);
 
-    private:
-        void init();
-        std::unique_ptr<QThread> spThread_;
-        std::shared_ptr<const PCMSettings> spSettings_;
-        struct CLData;
-        std::unique_ptr<CLData> spCLData_;
-        std::size_t clPlatId_, clDeviceId_;
-    };
+private:
+    void init();
+    std::unique_ptr<QThread> spThread_;
+    std::shared_ptr<const PCMSettings> spSettings_;
+    struct CLData;
+    std::unique_ptr<CLData> spCLData_;
+    std::size_t clPlatId_, clDeviceId_;
+};
 
 }
 
